e6cba9ebaafd31a4fc461e215592d02e07ec2eea
Vakantanka <laszlo.fenyvesi@gmail.com>
2022-01-11 14:15:45 +0100

    setHistory OK

--- practise1/src/App.js
+++ practise1/src/App.js
@@ -2,20 +2,25 @@ import { useState } from "react"
 
 const App = () => {
 
-  const [ shouldShowLanding, setSholdShowLanding ] = useState(true)
+  // const [ shouldShowLanding, setSholdShowLanding ] = useState(true)
   
   const [ pageName, setPageName ] = useState("Home");
+
+  const setHistory = (url) => { window.history.pushState({}, '', url) }
+  setHistory(pageName);
 
   return (
     <>
     <h1>Hello World</h1>
-    <button onClick={ () => setSholdShowLanding(!shouldShowLanding) }>toggle</button>
-    <button onClick={ () => setPageName("Home") }>Home</button>
-    <button onClick={ () => setPageName("About") }>About</button>
-    <button onClick={ () => setPageName("Landing") }>Contact</button>
-    <div>
+    {/* <button onClick={ () => setSholdShowLanding(!shouldShowLanding) }>toggle</button> */}
+    <nav>
+      <button onClick={ () => setPageName("Home") }>Home</button>
+      <button onClick={ () => setPageName("About") }>About</button>
+      <button onClick={ () => setPageName("Landing") }>Contact</button>
+    </nav>
+    {/* <div>
       {shouldShowLanding ? <div className="landing">landing</div> : <div className="home">Home</div>}
-    </div>
+    </div> */}
     <div>
       {pageName === "Home" && <div>Homepage component</div>}
       {pageName === "About" && <div>About component</div>}
